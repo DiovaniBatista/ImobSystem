80a51587a9714ced1a98cae9a3200c4578f916d6
EversonMoisesdev <eversonmoises301020@gmail.com>
2023-09-19 18:08:39 -0300

    Dapper

---
+++ Back/ImobSystem.Infra/Repository/ProdutoRepository.cs
@@ -0,0 +1,15 @@
+﻿using ImobSystem.Infra.Context;
+using ImobSystem.Infra.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImobSystem.Infra.Repository
+{
+    public class ProdutoRepository : IProdutoRepository
+    {
+        
+    }
+}
---
+++ Back/ImobySystem/Controllers/ProdutoController.cs
@@ -0,0 +1,34 @@
+﻿using ImobSystem.Domain.DTO;
+using ImobSystem.Infra.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ImobySystem.Controllers
+{
+    public class ProdutoController : ControllerBase
+    {
+        private readonly IProdutoService _produtoService;
+        public ProdutoController(IProdutoService produtoService)
+        {
+            _produtoService = produtoService;
+        }
+        [HttpPost("Create")]
+        public IActionResult Create(ProdutoCreateDto entrada)
+        {
+            var result = _produtoService.Create(entrada);
+            return Ok(result);
+        }
+        [HttpGet("GetAll")]
+        public IActionResult GetAll()
+        {
+            var result = _produtoService.GetAll();
+            return Ok(result);
+        }
+        [HttpGet("GetById")]
+        public IActionResult GetById(int id)
+        {
+            var result = _produtoService.GetById(id);
+            return Ok(result);
+        }
+
+    }
+}
